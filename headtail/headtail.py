f59f84915d8fcb94f55908d36e7ff9332a928e0d
rahil <rahilsolanki1014@gmail.com>
2022-06-01 22:57:56 +0530

    initial commit

---
+++ headtail/headtail.py
@@ -0,0 +1,32 @@
+import random
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print(''' 
+                                                   
+88                                         88  
+88                                         88  
+88                                         88  
+88,dPPYba,   ,adPPYba, ,adPPYYba,  ,adPPYb,88  
+88P'    "8a a8P_____88 ""     `Y8 a8"    `Y88  
+88       88 8PP""""""" ,adPPPPP88 8b       88  
+88       88 "8b,   ,aa 88,    ,88 "8a,   ,d88  
+88       88  `"Ybbd8"' `"8bbdP"Y8  `"8bbdP"Y8  
+                                               
+    
+    ''')
+else:
+    print('''
+                                                                          
+         88                        88                          88 88  
+         88                        88         ,d               "" 88  
+         88                        88         88                  88  
+ ,adPPYb,88 88       88  ,adPPYba, 88   ,d8 MM88MMM ,adPPYYba, 88 88  
+a8"    `Y88 88       88 a8"     "" 88 ,a8"    88    ""     `Y8 88 88  
+8b       88 88       88 8b         8888[      88    ,adPPPPP88 88 88  
+"8a,   ,d88 "8a,   ,a88 "8a,   ,aa 88`"Yba,   88,   88,    ,88 88 88  
+ `"8bbdP"Y8  `"YbbdP'Y8  `"Ybbd8"' 88   `Y8a  "Y888 `"8bbdP"Y8 88 88  
+                                                                      
+                                                                      
+    
+    ''')
